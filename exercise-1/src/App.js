567cecfc41a292d02e9b59abfc10211930b40692
Charles Burgess <consulting@cvburgess.com>
2019-10-23 09:31:55 -0400

    Update App.js

--- exercise-1/src/App.js
+++ exercise-1/src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import { ApiProvider } from "react-use-api";
 
 import AddTask from "./AddTask";
 import Navigation from "./Navigation";
 import Tasks from "./Tasks";
 
 const App = () => (
-  <ApiProvider>
-    <Navigation />
-    <Grid container spacing={1}>
-      <Tasks />
-      <AddTask />
-    </Grid>
-  </ApiProvider>
+  <Navigation />
+  <Grid container spacing={1}>
+    <Tasks />
+    <AddTask />
+  </Grid>
 );
 
 export default App;
