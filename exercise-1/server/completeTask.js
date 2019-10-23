c027123c5637c128654718b8049c008b0daa0d8c
Charles Burgess <consulting@cvburgess.com>
2019-10-23 10:17:53 -0400

    add support for completing tasks

--- exercise-1/server/completeTask.js
+++ exercise-1/server/completeTask.js
@@ -4,10 +4,15 @@ const pg = require("pg-promise")();
 const db = pg(process.env.DB_URL);
 
 exports.handler = async (event, context) => {
-  const tasks = await db.any("SELECT * FROM task WHERE is_complete = false");
+  const { id } = event.queryStringParameters;
+
+  const task = await db.none(
+    "UPDATE task SET is_complete=TRUE WHERE id=${id}",
+    { id }
+  );
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ tasks })
+    body: JSON.stringify(task)
   };
 };
